cd3495960bb34016e2e8fe5650a322a7080f4d72
jackteasell <contact.jackteasell@gmail.com>
2020-03-27 22:22:55 -0700

    polished up 1.py

--- 1.py
+++ 1.py
@@ -3,10 +3,13 @@ Practicepython.org problem #1
 Goal: Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 """
 import datetime
-name = input("What is your name?")
-age = int(input("What is your age?"))
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
 today = datetime.datetime.now()
-print("You will be 100 years old in the year: " , 100+((today.year) - age))
+thisYear = input("Did you already have your birthday this year?[y/n]: ")
+if (thisYear == "n"):
+    age += 1
+print(name,"you will be 100 years old in the year: " , 100+((today.year) - age))
 
 
 
